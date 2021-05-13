f526fe0b089a36b5a2d6892d553c5861060aa404
Mihir Vaishnav <70325171+MihirVaishnav@users.noreply.github.com>
2021-05-13 14:38:57 +0530

    Add files via upload

---
+++ js/careers.js
@@ -0,0 +1,118 @@
+
+  $(document).ready(function(){
+    $(".r1 h2").click(function(){
+      $(".r1 h2").text("-");
+      $(".r1").css({"padding-bottom":"3rem","transition":"padding-bottom .5s"});
+      $(".r1 h2").css({"border":"2px solid blue","color":"blue","border-right":"none"});
+      $(".r1 h1").css({"border":"2px solid blue","color":"blue"});
+    });
+  });
+  $(document).ready(function(){
+    $(".r1 h2").dblclick(function(){
+      $(".r1 h2").text("+");
+      $(".r1").css({"padding-bottom":"1rem","transition":"padding-bottom .5s"});
+      $(".r1 h2").css({"border":"2px solid rgba(0,0,0,0.5)","color":"rgba(0,0,0,0.4)","border-right":"none"});
+      $(".r1 h1").css({"border":"2px solid rgba(0,0,0,0.5)","color":"black"});
+    });
+  });
+
+
+
+  
+  $(document).ready(function(){
+    $(".r2 h2").click(function(){
+      $(".r2 h2").text("-");
+      $(".r2").css({"padding-bottom":"3rem","transition":"padding-bottom .5s"});
+      $(".r2 h2").css({"border":"2px solid blue","color":"blue","border-right":"none"});
+      $(".r2 h1").css({"border":"2px solid blue","color":"blue"});
+    });
+  });
+  $(document).ready(function(){
+    $(".r2 h2").dblclick(function(){
+      $(".r2 h2").text("+");
+      $(".r2").css({"padding-bottom":"1rem","transition":"padding-bottom .5s"});
+      $(".r2 h2").css({"border":"2px solid rgba(0,0,0,0.5)","color":"rgba(0,0,0,0.4)","border-right":"none"});
+      $(".r2 h1").css({"border":"2px solid rgba(0,0,0,0.5)","color":"black"});
+    });
+  });
+
+
+
+
+  $(document).ready(function(){
+    $(".r3 h2").click(function(){
+      $(".r3 h2").text("-");
+      $(".r3").css({"padding-bottom":"3rem","transition":"padding-bottom .5s"});
+      $(".r3 h2").css({"border":"2px solid blue","color":"blue","border-right":"none"});
+      $(".r3 h1").css({"border":"2px solid blue","color":"blue"});
+    });
+  });
+  $(document).ready(function(){
+    $(".r3 h2").dblclick(function(){
+      $(".r3 h2").text("+");
+      $(".r3").css({"padding-bottom":"1rem","transition":"padding-bottom .5s"});
+      $(".r3 h2").css({"border":"2px solid rgba(0,0,0,0.5)","color":"rgba(0,0,0,0.4)","border-right":"none"});
+      $(".r3 h1").css({"border":"2px solid rgba(0,0,0,0.5)","color":"black"});
+    });
+  });
+
+
+
+
+  
+  $(document).ready(function(){
+    $(".r4 h2").click(function(){
+      $(".r4 h2").text("-");
+      $(".r4").css({"padding-bottom":"3rem","transition":"padding-bottom .5s"});
+      $(".r4 h2").css({"border":"2px solid blue","color":"blue","border-right":"none"});
+      $(".r4 h1").css({"border":"2px solid blue","color":"blue"});
+    });
+  });
+  $(document).ready(function(){
+    $(".r4 h2").dblclick(function(){
+      $(".r4 h2").text("+");
+      $(".r4").css({"padding-bottom":"1rem","transition":"padding-bottom .5s"});
+      $(".r4 h2").css({"border":"2px solid rgba(0,0,0,0.5)","color":"rgba(0,0,0,0.4)","border-right":"none"});
+      $(".r4 h1").css({"border":"2px solid rgba(0,0,0,0.5)","color":"black"});
+    });
+  });
+
+
+
+  
+  $(document).ready(function(){
+    $(".r5 h2").click(function(){
+      $(".r5 h2").text("-");
+      $(".r5").css({"padding-bottom":"3rem","transition":"padding-bottom .5s"});
+      $(".r5 h2").css({"border":"2px solid blue","color":"blue","border-right":"none"});
+      $(".r5 h1").css({"border":"2px solid blue","color":"blue"});
+    });
+  });
+  $(document).ready(function(){
+    $(".r5 h2").dblclick(function(){
+      $(".r5 h2").text("+");
+      $(".r5").css({"padding-bottom":"1rem","transition":"padding-bottom .5s"});
+      $(".r5 h2").css({"border":"2px solid rgba(0,0,0,0.5)","color":"rgba(0,0,0,0.4)","border-right":"none"});
+      $(".r5 h1").css({"border":"2px solid rgba(0,0,0,0.5)","color":"black"});
+    });
+  });
+
+
+
+
+  $(document).ready(function(){
+    $(".r6 h2").click(function(){
+      $(".r6 h2").text("-");
+      $(".r6").css({"padding-bottom":"3rem","transition":"padding-bottom .5s"});
+      $(".r6 h2").css({"border":"2px solid blue","color":"blue","border-right":"none"});
+      $(".r6 h1").css({"border":"2px solid blue","color":"blue"});
+    });
+  });
+  $(document).ready(function(){
+    $(".r6 h2").dblclick(function(){
+      $(".r6 h2").text("+");
+      $(".r6").css({"padding-bottom":"1rem","transition":"padding-bottom .5s"});
+      $(".r6 h2").css({"border":"2px solid rgba(0,0,0,0.5)","color":"rgba(0,0,0,0.4)","border-right":"none"});
+      $(".r6 h1").css({"border":"2px solid rgba(0,0,0,0.5)","color":"black"});
+    });
+  });
